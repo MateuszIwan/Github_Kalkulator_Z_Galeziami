4d5e26c86e11ac592909cd2c3ccfce8b65cae5bf
4TP Mateusz Iwan <4TP Mateusz Iwan@S27K106>
2024-12-05 09:28:18 +0100

    Dodaj pliki projektów.

---
+++ Github_Kalkulator_Z_Galeziami/Kalkulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github_Kalkulator_Z_Galeziami
+{
+    public static partial class Kalkulator
+    {
+        public static int Kwadrat(int a)
+        {
+            return a * a;
+        }
+    }
+}
